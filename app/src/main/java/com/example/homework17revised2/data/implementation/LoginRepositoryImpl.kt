65faa7a54e64c14b7687d9b0be2b10e1c35c08e4
nikoloz <nikolozkaladze@gmail.com>
2023-12-24 14:38:56 -0800

    added error handling on login side

--- app/src/main/java/com/example/homework17revised2/data/implementation/LoginRepositoryImpl.kt
+++ app/src/main/java/com/example/homework17revised2/data/implementation/LoginRepositoryImpl.kt
@@ -3,13 +3,16 @@ package com.example.homework17revised2.data.implementation
 import android.net.http.HttpException
 import android.util.Log.d
 import com.example.homework17revised2.common.Resource
+import com.example.homework17revised2.data.dto.LoginErrorResponseDto
 import com.example.homework17revised2.data.dto.toDomain
 import com.example.homework17revised2.data.dto.toDto
 import com.example.homework17revised2.data.service.LoginService
 import com.example.homework17revised2.domain.login.LoginRepository
 import com.example.homework17revised2.domain.login.LoginRequest
 import com.example.homework17revised2.domain.login.LoginResponse
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import okhttp3.ResponseBody
@@ -26,18 +29,35 @@ class LoginRepositoryImpl @Inject constructor(val loginService: LoginService) : 
                     emit(Resource.Success(data = response.body()!!.toDomain()))
                 }
                 else {
-                    emit(Resource.Error(errorMessage = response.errorBody()?.toString() ?: ""))
+                    val errorBody = response.errorBody()?.string() ?: ""
+                    val errorResponseDto = parseErrorBody(errorBody)
+                    val errorMessage = errorResponseDto?.toDomain()?.error ?: "Unknown error"
+                    emit(Resource.Error(errorMessage = errorMessage))
                 }
             }
             catch (e:IOException){
-                d("LoginRepositoryImplementation", "${e.message}")
+                emit(Resource.Error(errorMessage = e.message ?: "Something went wrong"))
             }
             catch (e:HttpException){
-                d("LoginRepositoryImplementation", "${e.message}")
+                emit(Resource.Error(errorMessage = e.message ?: "Please check your network connection"))
             }
             catch (e:Exception){
-                d("LoginRepositoryImplementation", "${e.message}")
+                emit(Resource.Error(errorMessage = "Something went wrong"))
             }
+        }
+    }
+
+    private fun parseErrorBody(errorBody: String): LoginErrorResponseDto? {
+        val moshi = Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+
+        val errorAdapter: JsonAdapter<LoginErrorResponseDto> = moshi.adapter(LoginErrorResponseDto::class.java)
+
+        return try {
+            errorAdapter.fromJson(errorBody)
+        } catch (e: Exception) {
+            null
         }
     }
 }
